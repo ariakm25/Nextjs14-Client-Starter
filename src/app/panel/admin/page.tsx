1096aad5848afeb89fe5a17721e3dd6c4cba327a
nrmadi02 <ahmadinur2202@gmail.com>
2024-01-21 15:26:37 +0800

    [feat]: add swr fetch

--- src/app/panel/admin/page.tsx
+++ src/app/panel/admin/page.tsx
@@ -3,6 +3,7 @@
 import { useAuthSlice } from '@/features/auth/auth.slice';
 import { Button } from '@/common/components/ui/button';
 import { signOut } from 'next-auth/react';
+import Link from 'next/link';
 
 export default function AdminPage() {
   const { user } = useAuthSlice();
@@ -12,10 +13,13 @@ export default function AdminPage() {
       <h2>
         HELLO Name: {user?.name} Role: {user?.role?.name}
       </h2>
-      <div>
+      <div className='flex gap-3'>
         <Button onClick={() => signOut()} variant="default">
           Logout
         </Button>
+        <Link href={'/panel/admin/users'}>
+          <Button variant="outline">List Users</Button>
+        </Link>
       </div>
     </div>
   );
