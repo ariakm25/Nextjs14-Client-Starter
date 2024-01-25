1c871d46ed976a98324f627067567835c613c364
Aria <ariakm25@gmail.com>
2024-01-25 13:37:27 +0700

    update packages

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -16,7 +16,7 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
+        {/* This is the default Next.js error component, but it doesn't allow omitting the statusCode property yet. */}
         <NextError statusCode={undefined as any} />
       </body>
     </html>
